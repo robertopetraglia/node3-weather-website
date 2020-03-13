f0e86ca398b7ab26ef746ed4878492f6618dbb71
rpetraglia <rpetraglia@alpenite.com>
2020-03-13 23:05:30 +0100

    Add new forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree out. There is a ' + body.currently.precipProbability + '% change of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree out. There is a ' + body.currently.precipProbability + '% change of rain. There is a ' + body.daily.data[0].humidity + '% of umidity and a visibility of ' + body.daily.data[0].visibility + '%')
         }
     })
 }
